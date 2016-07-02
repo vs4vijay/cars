eb36eb33a12a18214aaf3c27e7c1a01eba2b7f64
vs4vijay <vs4vijay@gmail.com>
2016-07-02 19:03:14 +0530

    Add Car model

---
+++ app/assets/javascripts/components/cars.js.coffee
@@ -0,0 +1,8 @@
+@Cars = React.createClass
+  render: ->
+    # React.DOM.div
+      # className: "cars"
+      # "Carss"
+    `<div className="cars">
+      Jazz cars
+    </div>`
---
+++ db/migrate/20160702124741_create_cars.rb
@@ -0,0 +1,11 @@
+class CreateCars < ActiveRecord::Migration[5.0]
+  def change
+    create_table :cars do |t|
+      t.string :model
+      t.string :brand
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
